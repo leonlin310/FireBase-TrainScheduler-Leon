dc8f83a91492c09e6a7d6dfd8ad96b130d826fda
Leon Lin <leonlin310@gmail.com>
2019-04-21 16:39:32 -0500

    initialize files

---
+++ assets/logic.js
@@ -0,0 +1,14 @@
+//TODO: Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyAa11wV-YuiKv5h1RG2Y5tUN364eFzoR6Y",
+    authDomain: "train-scheduler-assignme-69c39.firebaseapp.com",
+    databaseURL: "https://train-scheduler-assignme-69c39.firebaseio.com",
+    projectId: "train-scheduler-assignme-69c39",
+    storageBucket: "train-scheduler-assignme-69c39.appspot.com",
+    messagingSenderId: "403935769131"
+  };
+  
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
