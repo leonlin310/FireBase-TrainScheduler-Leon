32c7dfa53f41612240605c8e3bd10927970a9ed3
Leon Lin <leonlin310@gmail.com>
2019-04-21 17:35:04 -0500

    pending to the DOM correctly

--- assets/logic.js
+++ assets/logic.js
@@ -7,15 +7,15 @@ var config = {
     projectId: "train-scheduler-assignme-69c39",
     storageBucket: "train-scheduler-assignme-69c39.appspot.com",
     messagingSenderId: "403935769131"
-  };
-  
-  firebase.initializeApp(config);
-  var database = firebase.database();
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
 
 
-  //DONE: Create an on click submit event
-$("#submitButton").on("click", function(){
-  //DONE: grab the values of info typed into fields and save to variable
+//DONE: Create an on click submit event
+$("#submitButton").on("click", function () {
+    //DONE: grab the values of info typed into fields and save to variable
     var trainVal = $("#trainNameField").val().trim();
     var destinationVal = $("#destinationField").val().trim();
     var startTimeVal = $("#startTimeField").val().trim();
@@ -34,6 +34,24 @@ $("#submitButton").on("click", function(){
 
 });
 
+//TODO: Grab info from the Firebase then append to the HTML
+database.ref().on("child_added", function (child) {
+    console.log(child.val());
+
+    var trainPulledVal = child.val().Name; 
+    var destinationPulledVal = child.val().Destination; 
+    var startTimePulledVal = child.val().TrainTime; 
+    var frequencyPulledVal = child.val().Frequency;
+    
+    var tableRow = $("<tr>").append(
+        $("<td>").text(trainPulledVal),
+        $("<td>").text(destinationPulledVal),
+        $("<td>").text(frequencyPulledVal),
+        $("<td>").text("Next Arrival TBD"),
+        $("<td>").text("Minutes Away TBD")
+    )
+    $("tbody").append(tableRow);
+});
 
 
 
